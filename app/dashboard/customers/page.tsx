b1fb53f691598e09ace7cddfe7a7032774f4e33e
Hafeez <muhammed.hafeez.nayazi@gmail.com>
2024-11-30 21:46:46 +0530

    5 chapters done

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      customer page
+    </div>
+  )
+}
+
+export default page
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+/**
+ * 
+ * invoice page for dashboard
+ */
+function page(): React.JSX.Element {
+  return (
+    <div>
+      invoices page
+    </div>
+  )
+}
+
+export default page
