ead47bf7d88dfe2650f4b308c3ff46a50cec0ea5
Hafeez <muhammed.hafeez.nayazi@gmail.com>
2024-12-01 15:43:53 +0530

    completed overview page

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,10 @@
+import DashboardSkeleton from '@/app/ui/skeletons'
+import React from 'react'
+
+function Loading() {
+    return (
+        <DashboardSkeleton />
+    )
+}
+
+export default Loading
--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,4 +2,4 @@ import { NextFont } from "next/dist/compiled/@next/font";
 import { Inter, Lusitana } from "next/font/google";
 
 export const inter: NextFont = Inter({ weight: "300", subsets: ["latin"] });
-export const lusitana: NextFont = Lusitana({ weight: "400" });
+export const lusitana: NextFont = Lusitana({ weight: "400", subsets: ["latin"] });
